f5eddce7cfce2a0a40accd445e086ce547c0946f
Ben McCormick <exponen7@gmail.com>
2020-05-14 11:38:23 +0800

    Updating ts

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@phobon/base';
+import { MDXProvider } from '@mdx-js/react';
+import { AnimatePresence } from 'framer-motion';
+
+const components = {
+  // Map components as required here. Move this to local state to update
+  // at runtime, which may not really be needed
+};
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <MDXProvider components={components}>
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </MDXProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+import GlobalStyles from './GlobalStyles';
+
+export default class TelestoDocument extends Document {
+}
+
+TelestoDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheet.collectStyles(
+        <>
+          <GlobalStyles />
+          <App {...props} />
+        </>
+      ),
+    });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        [
+          ...React.Children.toArray(initialProps.styles),
+          sheet.getStyleElement(),
+        ]
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+}
